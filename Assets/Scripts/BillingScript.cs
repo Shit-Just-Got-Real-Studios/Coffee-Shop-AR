42451bbeb71ec124fd9983ce3e98b5ccb0681fd8
l0fty-WhizZ <shreyas@shitjustgotreal.in>
2016-10-17 02:01:01 +0530

    Added text prefab

--- Assets/Scripts/BillingScript.cs
+++ Assets/Scripts/BillingScript.cs
@@ -14,6 +14,7 @@ public class BillingScript : MonoBehaviour {
 	public List<Text> itemInfo;
 	public Text t1, t2, t3, rft;
 	public Transform canvasTransform;
+	public List<Text> textPrefabs;
 	public void AddQuantity() {
 		ItemScript item = currentItem.GetComponent<ItemScript> ();
 		item.itemQuantity += 1;
@@ -25,29 +26,12 @@ public class BillingScript : MonoBehaviour {
 		item.itemQuantity -= 1;
 		totalCost -= item.itemCost;
 	}
-
-	public void addToReceipt() {
-
-	}
-
-	GameObject CreateText(Transform canvas_transform, float x, float y, string text_to_print)
-	{
-		GameObject UItextGO = new GameObject("Text2");
-		UItextGO.transform.SetParent(canvas_transform);
-
-		RectTransform trans = UItextGO.AddComponent<RectTransform>();
-		trans.anchoredPosition = new Vector2(x, y);
-
-		Text text = UItextGO.AddComponent<Text>();
-		text.text = text_to_print;
-		return UItextGO;
-	}
-
+		
 	public void showReceipt() {
 		receipt.SetActive (true);
 		int j = 0;
 		int i = 0;
-		while (i < boughtItems.Count) {
+		while (i < boughtItems.Count * 3) {
 			ItemScript tempitem = boughtItems[j].GetComponent<ItemScript> ();
 			itemInfo [i].transform.position = new Vector3 (rft.transform.position.x - 50, rft.transform.position.y - (j) * 30, rft.transform.position.z);
 			itemInfo [i].text = "" + tempitem.itemName;
@@ -57,9 +41,6 @@ public class BillingScript : MonoBehaviour {
 			itemInfo [i + 2].text = "" + tempitem.itemCost;
 			j++;
 			i += 3;
-			//GameObject itemName = CreateText (canvasTransform, rft.transform.position.x - 50, rft.transform.position.y - (i) * 30, ""+tempitem.itemName);
-			//GameObject itemQuantity = CreateText (canvasTransform, rft.transform.position.x + 200, rft.transform.position.y - (j) * 30, ""+tempitem.itemQuantity);
-			//GameObject itemCost = CreateText (canvasTransform, rft.transform.position.x + 400, rft.transform.position.y - (j) * 30, ""+tempitem.itemCost);
 		}
 	}
 
@@ -67,16 +48,9 @@ public class BillingScript : MonoBehaviour {
 		ItemScript item = currentItem.GetComponent<ItemScript> ();
 		if (item.itemQuantity >= 1 && !boughtItems.Contains(currentItem)) {
 			boughtItems.Add (currentItem);
-			itemInfo.Add (t1);
-			itemInfo.Add (t2);
-			itemInfo.Add (t3);
-
-			/*
-			ItemScript tempitem = boughtItems[index].GetComponent<ItemScript> ();
-			itemName.text = ""+tempitem.itemName;
-			itemQuantity.text = ""+tempitem.itemQuantity;
-			itemCost.text = ""+tempitem.itemCost;
-			*/
+			for (int i = 0; i < 18; i++) {
+				itemInfo.Add (textPrefabs [i]);
+			}
 			index++;
 		}
 
